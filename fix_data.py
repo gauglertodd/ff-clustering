d99ca462f2d7047051d61f3732fa3b31ff1a0249
Todd <gauglertodd@gmail.com>
2016-07-02 13:01:02 -0400

    Saving rankings
    Added function to save info in np arrays

--- fix_data.py
+++ fix_data.py
@@ -1,18 +1,40 @@
 ''' This script trims useless data from the EC rankings, and
 makes sure that some useful data is added to our pandas DF '''
-import csv
 import os
-import pandas as pd
-from pandas import DataFrame
+import numpy as np
 
 
-def trim_rankings():
-    ''' this function does stuff '''
+def save_rankings():
+    ''' this function "cleans" and stores EC rankings'''
     directory = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              "Rankings")
     file_names = os.listdir(directory)
-    with open(directory + "/"  + file_names[1], 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            print ', '.join(row)
-trim_rankings()
+    # 0: Rank
+    # 1: Player Name
+    # 2: Position
+    # 3: Team
+    # 4: Bye Week
+    # 5: Best Rank
+    # 6: Worst Rank
+    # 7: Avg Rank
+    # 8: Std Dev
+    # 9: ADP
+    save_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                  "Saved")
+
+    for i in range(1, len(file_names)):
+        filename = directory + "/" + file_names[i]
+        save_file_name = save_directory + "/" + file_names[i][:-4] + ".npy"
+        thedata = np.genfromtxt(
+            filename,                 # file name
+            skip_header=5,          # lines to skip at the top
+            skip_footer=0,          # lines to skip at the bottom
+            delimiter='\t',         # column delimiter
+            dtype='string',         # data type
+            filling_values=0,       # fill missing values with 0
+            usecols=(1, 5, 6, 7, 8, 9),  # columns to read
+            )     # column names
+        np.save(save_file_name, thedata)
+
+
+save_rankings()
